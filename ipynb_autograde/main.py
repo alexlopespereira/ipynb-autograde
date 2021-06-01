e374fb70e8603cf7771d02500f6d21ae246cbb99
alex <alex.pereira@planejamento.gov.br>
2021-06-01 18:17:03 -0300

    incremental improvements

--- ipynb_autograde/main.py
+++ ipynb_autograde/main.py
@@ -4,7 +4,11 @@ import autograde
 def init_log():
     autograde.init_log()
 
+
 def validate(func, inputs, outfunc, outputs, exercise_number):
     return autograde.validate(func, inputs, outfunc, outputs, exercise_number)
 
+
+def validate2(func, inputs, outfunc, outputs, exercise_number):
+    return autograde.validate2(func, inputs, outfunc, outputs, exercise_number)
 
