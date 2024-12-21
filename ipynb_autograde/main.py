abe87703764f0bc6081449f9582cd144be7cbe7b
Alex Lopes Pereira <alexlopespereira@gmail.com>
2024-12-21 14:15:04 -0400

    incremental improvements

--- ipynb_autograde/main.py
+++ ipynb_autograde/main.py
@@ -5,10 +5,10 @@ def init_log():
     autograde.init_log()
 
 
-def validate(func, inputs, outfunc, outputs, exercise_number):
-    return autograde.validate(func, inputs, outfunc, outputs, exercise_number)
+def validate(prompt, exercise_number):
+    return autograde.validate(prompt, exercise_number)
 
 
-def validate2(func, inputs, outfunc, outputs, exercise_number):
-    return autograde.validate2(func, inputs, outfunc, outputs, exercise_number)
+# def validate2(func, inputs, outfunc, outputs, exercise_number):
+#     return autograde.validate2(func, inputs, outfunc, outputs, exercise_number)
 
